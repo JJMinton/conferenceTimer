4f1a9c01be5ed2c95454f1cb1e0d233bd4b7bdb5
JJMinton <jeremyminton@gmail.com>
2017-01-07 22:09:52 +0000

    light controls now run RPi.GPIO pins

--- light_controls.py
+++ light_controls.py
@@ -1,75 +1,76 @@
-import asyncio
-import time
+import asyncio;
+import time;
 
-import RPi.GPIO as io
+import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+gpio.setup(23, gpio.OUT)
+gpio.setup(24, gpio.OUT)
+gpio.setup(25, gpio.OUT)
+
+def switch_light(cmd_str):
+    if cmd_str[0] == 'g':
+        gpio.output(23, 0)
+    elif cmd_str[0] == '-':
+        gpio.output(23, 1)
+    else:
+        raise ValueError('incorrect input for green light')
+    if cmd_str[1] == 'o':
+        gpio.output(24, 0)
+    elif cmd_str[1] == '-':
+        gpio.output(24, 1)
+    else:
+        raise ValueError('incorrect input for orange light')
+    if cmd_str[2] == 'r':
+        gpio.output(25, 0)
+    elif cmd_str[2] == '-':
+        gpio.output(25, 1)
+    else:
+        raise ValueError('incorrect input for red light')
+
 
 from controller import Controller
 
-io.setmode(io.BOARD)
-io.setup(11, io.OUT)
-io.setup(13, io.OUT)
-io.setup(15, io.OUT)
-
-def green(on=True):
-    io.output(11, on)
-def orange(on=True):
-    io.output(13, on)
-def red(on=True):
-    io.output(15, on)
-def lights(state):
-    if type(state) == int:
-        green(bool(state/100))
-        orange(bool(state/10 % 10))
-        red(bool(state % 10))
-    if type(state) == tuple:
-        green(state[0])
-        orange(state[1])
-        red(state[2])
-    if type(state) == str:
-        green(state[0] in ['1','g'])
-        orange(state[1]  in ['1', 'o'])
-        red(state[1] in ['1', 'r'])
-
 async def starting():
     while True:
-        lights('-or')
+        switch_light('-or');
         await asyncio.sleep(2.0);
 async def speaking():
     while True:
-        lights('g--');
+        switch_light('g--');
         await asyncio.sleep(2.0);
 async def speakingwarning():
     while True:
-        lights('go-');
+        switch_light('go-');
         await asyncio.sleep(2.0);
 async def questions():
     while True:
-        lights('-o-');
+        switch_light('-o-');
         await asyncio.sleep(2.0);
 async def questionswarning():
     while True:
-        lights('---');
+        switch_light('---');
         await asyncio.sleep(1.0);
-        lights('-o-');
+        switch_light('-o-');
         await asyncio.sleep(1.0);
 async def change():
     while True:
-        lights('--r');
+        switch_light('--r');
         await asyncio.sleep(2.0);
 def clear():
-    lights('---');
+    switch_light('---');
 
 
 if __name__ == '__main__':
 
     light_control = Controller(universal_callbacks=clear)
     light_control.start(starting())
-    light_control.sleep(2.1)
+    light_control.sleep(4.1)
     light_control.stop_all()
     light_control.start(speaking())
-    light_control.sleep(3)
+    light_control.sleep(6)
     light_control.stop_all()
     light_control.start(questionswarning())
-    light_control.sleep(2)
+    light_control.sleep(4)
     light_control.stop_all()
 
