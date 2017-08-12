194838821922e788cf2698cfaade78e0e25de462
JJMinton <jeremyminton@gmail.com>
2017-08-12 13:48:50 +0100

    copy time stamp with rsync script. This should prevent unnecessary updates of the schedule.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync jjm61@jjm.nz:~/conferenceTimer/ $HOME/conferenceTimer -r --inplace --exclude-from=$HOME/conferenceTimer/scripts/sync_exclude.txt
+rsync jjm61@jjm.nz:~/conferenceTimer/ ~/conferenceTimer -rt --delay-updates --exclude-from=~/conferenceTimer/scripts/sync_exclude.txt
