1f7ff3b07c710715d1b314429500b14239bae84d
JJMinton <jeremyminton@gmail.com>
2017-08-13 10:34:48 +0100

    fake schedule generator.

---
+++ generate_fake_schedule.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from datetime import datetime, timedelta
+start_time = datetime.now()
+talk_length = 4
+question_length = 2
+date_format = '%d/%m/%Y:%H%M'
+
+room_list = ['ictp-rpi-1',
+             'ictp-rpi-2',]
+
+
+talks =[('Green Lantern','Well that was a rubbish Movie'),
+        ('Deadpool','R18 super hero'),
+        ('Rogers','Marvel super Heroes'),
+        ('Bucky Ball winner','Marvel Bad Guys'),
+        ('The Joker','DC Bad Guys'),
+        ('Batman','DC Good Guys'),
+        ('Spiderman',"DC can't actually fly"),
+        ('Superman','DC can fly'),
+        ('Hulk','Marvel jump good'),
+        ('Ironman','Marvel Can actually Fly'),
+        ('Black widow','Marvel lady Avenger'),
+        ('The Flash','Super fast hero'),]
+
+id_number = 0
+df = pd.DataFrame(columns=['id', 'room_code', 'start_time', 'talk_length', 'question_length', 'chair', 'name', 'title'])
+for room in room_list:
+    for name, title in talks:
+        df = df.append({
+                        'start_time': start_time.strftime(date_format),
+                        'id': id_number,
+                        'chair': 'Paul Blart',
+                        'room_code': room,
+                        'talk_length': talk_length,
+                        'question_length': question_length,
+                        'name': name,
+                        'title': title,
+                       }, ignore_index=True)
+        id_number += 1
+        start_time += timedelta(minutes=talk_length+question_length)
+            
+df.to_csv('schedule.csv', index=False, float_format='%.0f')
