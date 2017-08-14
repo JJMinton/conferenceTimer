06fd3fa419b67cfa48348165e93d0d8a6e7a6ccd
JJMinton <jeremyminton@gmail.com>
2017-08-14 09:40:11 +0100

    tidied up screen layout

--- screen_controls.py
+++ screen_controls.py
@@ -24,28 +24,43 @@ class Screen():
                      72: tkFont.Font(family='Helvetica', size=72, weight='bold'),
                     }
 
+        #Create color bars
+        #w,h = self.root.winfo_width(), self.root.winfo_height()
+        #self.canvas = tk.Canvas(self.root, borderwidth=0, highlightThickness=0)
+        #self.canvas.grid(row=0,column=0)
+        self.top_color = tk.Label(self.root, bg='yellow')
+        self.top_color.pack(fill=tk.BOTH, expand=True)
+        self.bottom_color = tk.Label(self.root, bg='magenta')
+        self.bottom_color.pack(fill=tk.BOTH, expand=True)
+
         #Create Text
-        #self.text_frame = tk.Frame(self.root, width=w, height=h, bg="red")
-        #self.text_frame.place(in_=self.root, anchor='c', relx=0.5, rely=0.5);#pack(side = tk.LEFT);#, width=200, height=200, background="red").pack();
-        self.text_frame = self.root
         self.speaker = tk.StringVar()
         self.title = tk.StringVar()
         self.message = tk.StringVar()
         self.labels = []
-        self.labels.append(tk.Label(self.text_frame, text='Speaker:', font=self.helv[24]))
-        self.labels.append(tk.Label(self.text_frame, textvariable=self.speaker, font=self.helv[36]))#place(_in=container);
-        self.labels.append(tk.Label(self.text_frame, text='Title:', font=self.helv[24]))#place(_in=container);
-        self.labels.append(tk.Label(self.text_frame, textvariable=self.title, font=self.helv[24]))#place(_in=container);
-        self.labels.append(tk.Label(self.text_frame, textvariable=self.message, font=self.helv[72]))#place(_in=container);
-        for l in self.labels:
-            l.pack()
+        self.labels.append(tk.Label(self.top_color, text='Speaker:', font=self.helv[24]))
+        self.labels.append(tk.Label(self.top_color, textvariable=self.speaker, font=self.helv[36]))#place(_in=container);
+        self.labels.append(tk.Label(self.top_color, text='Title:', font=self.helv[24]))#place(_in=container);
+        self.labels.append(tk.Label(self.top_color, textvariable=self.title, font=self.helv[24]))#place(_in=container);
+        self.labels.append(tk.Label(self.bottom_color, textvariable=self.message, font=self.helv[72]))#place(_in=container);
+        for i, l in enumerate(self.labels[:-1]):
+            l.pack(anchor=tk.CENTER)
+        self.labels[-1].place(rely=.5, relx=.5, anchor=tk.CENTER)
         self.root.update()
 
-    def update(self, name=None, title=None, message=None, color=None):
-        if color is not None:
-            self.text_frame.configure(background=color)
-            for l in self.labels:
+    def update(self, name=None, title=None, message=None, color=None, color2=None):
+        if color is None:
+            color = 'grey'
+        if color2 is None:
+            color2 = color
+        if self.top_color['bg'] != color:
+            self.top_color.configure(background=color)
+            for l in self.labels[:-1]:
                 l.config(bg=color)
+        if self.bottom_color['bg'] != color2:
+            self.bottom_color.configure(background=color2)
+            self.labels[-1].config(bg=color2)
+
         self.speaker.set('' if name is None else name)
         self.title.set('' if title is None else title)
         self.message.set('' if message is None else message)
@@ -64,61 +79,51 @@ class Screen():
     def starting(self, name=None, title=None, endTime=None):
         async def starting(loop):
             while True:
-                self.update(name, title, 'Starting in: {}'.format(formatted_count_down(endTime)), 'orange')
-                await asyncio.sleep(1., loop=loop)
-                self.update(name, title, 'Starting in: {}'.format(formatted_count_down(endTime)), 'grey')
-                await asyncio.sleep(1., loop=loop)
+                self.update(name, title, 'Starting in: {}'.format(formatted_count_down(endTime)), 'orange', 'grey')
+                await asyncio.sleep(1.0, loop=loop)
+                self.update(name, title, 'Starting in: {}'.format(formatted_count_down(endTime)), 'grey', 'orange')
+                await asyncio.sleep(1.0, loop=loop)
         return starting
 
     def speaking(self, name=None, title=None, endTime=None):
         async def speaking(loop):
             while True:
                 self.update(name, title, 'Speaking for: {}'.format(formatted_count_down(endTime)), 'green')
-                await asyncio.sleep(1.0, loop=loop)
+                await asyncio.sleep(0.01, loop=loop)
         return speaking
 
     def speaking_warning(self, name=None, title=None, endTime=None):
         async def speakingwarning(loop):
             while True:
-                self.update(name, title, 'Speaking for: {}'.format(formatted_count_down(endTime)), 'green')
-                await asyncio.sleep(1.0, loop=loop)
-                
-                self.update(name, title, 'Speaking for: {}'.format(formatted_count_down(endTime)), 'orange')
-                await asyncio.sleep(1.0, loop=loop)
+                self.update(name, title, 'Speaking for: {}'.format(formatted_count_down(endTime)), 'green', 'orange')
+                await asyncio.sleep(0.01, loop=loop)
         return speakingwarning
 
     def questions(self, name=None, title=None, endTime=None):
         async def questions(loop):
             while True:
                 self.update(name, title, 'Questions for: {}'.format(formatted_count_down(endTime)), 'orange')
-                await asyncio.sleep(1.0, loop=loop)
+                await asyncio.sleep(0.01, loop=loop)
         return questions
 
     def questions_warning(self, name=None, title=None, endTime=None):
         async def questions(loop):
             while True:
-
-                self.update(name, title, 'Questions for: {}'.format(formatted_count_down(endTime)), 'orange')
-                await asyncio.sleep(1.0, loop=loop)
-
-                self.update(name, title, 'Questions for: {}'.format(formatted_count_down(endTime)), 'red')
-                await asyncio.sleep(1.0, loop=loop)
+                self.update(name, title, 'Questions for: {}'.format(formatted_count_down(endTime)), 'orange', 'red')
+                await asyncio.sleep(0.01, loop=loop)
         return questions
     def empty_schedule(self):
         async def questions(loop):
             while True:
-                self.update('', '', "That's it, there's no more!", random.choice(['red','orange','green']))
-                await asyncio.sleep(1.0, loop=loop)
+                self.update('', '', "That's it, there's no more", random.choice(['red','orange','green']), random.choice(['red','orange','green']))
+                await asyncio.sleep(0.4, loop=loop)
         return questions
 
 def formatted_count_down(end_time):
     dt = (end_time - datetime.now()).total_seconds()
     hours = math.floor(dt/(60*60))
-    print(hours)
     minutes = math.floor((dt%(60*60))/60)
-    print(minutes)
     seconds = math.floor(dt%(60))
-    print(seconds)
     if hours > 0:
         return '{:}:{:0>2}:{:0>2}'.format(hours, minutes, seconds)
     else:
